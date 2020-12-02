47ecd37195dbd1a6af7c669773588fee5be3edee
Iain M Norman <iain@earthware.co.uk>
2020-12-02 09:11:55 +0000

    Day 3 prep

---
+++ Day3/Day3.cs
@@ -0,0 +1,17 @@
+﻿namespace Aoc2020.Day3
+{
+    using System;
+
+    public class Day3 : IDay
+    {
+        public string ExecutePart1(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExecutePart2(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,34 +25,37 @@
             var day = (IDay)Activator.CreateInstance(dayType);
             using var http = new HttpClient();
 
+            string input;
+
             try
             {
-                var input = await http.GetStringAsync($"https://aocproxy.azurewebsites.net/2020/day/{argDay}/input");
-                var stopwatch = new Stopwatch();
-                var totalMs = 0L;
-                Console.WriteLine("Day " + argDay);
-                Console.WriteLine("Starting part 1");
-                stopwatch.Start();
-                var p1 = day.ExecutePart1(input);
-                stopwatch.Stop();
-                Console.WriteLine("Part 1 answer: " + p1);
-                totalMs += stopwatch.ElapsedMilliseconds;
-                Console.WriteLine($"Part 1 took : {stopwatch.ElapsedMilliseconds}ms");
-                Console.WriteLine("Starting part 2");
-                stopwatch.Reset();
-                stopwatch.Start();
-                var p2 = day.ExecutePart2(input);
-                stopwatch.Stop();
-                Console.WriteLine("Part 1 answer: " + p2);
-                Console.WriteLine($"Part 2 took : {stopwatch.ElapsedMilliseconds}ms");
-                totalMs += stopwatch.ElapsedMilliseconds;
-                Console.WriteLine($"Day {argDay} took {totalMs}ms");
+                input = await http.GetStringAsync($"https://aocproxy.azurewebsites.net/2020/day/{argDay}/input");                
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"{ex.Message}");
                 return;
             }
+
+            var stopwatch = new Stopwatch();
+            var totalMs = 0L;
+            Console.WriteLine("Day " + argDay);
+            Console.WriteLine("Starting part 1");
+            stopwatch.Start();
+            var p1 = day.ExecutePart1(input);
+            stopwatch.Stop();
+            Console.WriteLine("Part 1 answer: " + p1);
+            totalMs += stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Part 1 took : {stopwatch.ElapsedMilliseconds}ms");
+            Console.WriteLine("Starting part 2");
+            stopwatch.Reset();
+            stopwatch.Start();
+            var p2 = day.ExecutePart2(input);
+            stopwatch.Stop();
+            Console.WriteLine("Part 1 answer: " + p2);
+            Console.WriteLine($"Part 2 took : {stopwatch.ElapsedMilliseconds}ms");
+            totalMs += stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Day {argDay} took {totalMs}ms");
         }
     }
 }
