d341c815fd3885685b540b1cbb010d1ebc28877c
Iain M Norman <iain@earthware.co.uk>
2020-12-06 08:17:32 +0000

    Day 7

---
+++ Aoc2020/Day7/Day7.cs
@@ -0,0 +1,24 @@
+﻿namespace Aoc2020.Day6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Day7 : IDay
+    {
+        public string ExecutePart1(string input)
+        {
+            var p1 = 0;
+
+            return p1.ToString();
+        }
+
+        public string ExecutePart2(string input)
+        {
+            var p2 = 0;
+
+            return p2.ToString();
+        }
+    }
+}
---
+++ Tests/Day7Tests.cs
@@ -0,0 +1,30 @@
+﻿using Aoc2020.Day6;
+
+using Shouldly;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Tests
+{
+    public class Day7Tests
+    {
+        public Day7Tests()
+        {
+            this.day = new Day7();
+        }
+
+        Day7 day;
+
+        [Fact]
+        public void P1T1()
+        {
+
+        }
+    }
+}
