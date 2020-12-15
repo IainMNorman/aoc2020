756f3afb62a751190787cb7938a44f99a66c7c54
Iain M Norman <iain@earthware.co.uk>
2020-12-15 19:04:33 +0000

    Day 15

---
+++ Aoc2020/Day15/Day15.cs
@@ -0,0 +1,61 @@
+﻿namespace Aoc2020.Day15
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day15 : IDay
+    {
+        public string ExecutePart1(string input)
+        {
+            var numbers = input.Split(',').Select((v, i) => new { index = (long)i, value = long.Parse(v) })
+                .ToDictionary(x => x.index, x => x.value);
+
+            var count = numbers.Count();
+
+            for (int i = count; i < 2020; i++)
+            {
+                var lastNumber = numbers[i - 1];
+
+                if (numbers.Count(x => x.Value == lastNumber) == 1)
+                {
+                    numbers.Add(i, 0);
+                }
+                else
+                {
+                    var lastTwo = numbers.Where(x => x.Value == lastNumber).OrderByDescending(x => x.Key).Take(2).ToArray();
+
+                    var newNumber = lastTwo[0].Key - lastTwo[1].Key;
+                    numbers.Add(i, newNumber);
+                }
+            }
+
+            return numbers[2019].ToString();
+        }
+
+        public string ExecutePart2(string input)
+        {
+            var numbers = input.Split(',').Select((v, i) => new { index = (long)i, value = long.Parse(v) })
+                .ToDictionary(x => x.value, x => x.index);
+
+            var lastNumber = numbers.Last().Key;
+
+            for (int i = numbers.Count(); i < 2020; i++)
+            {
+                if (numbers[lastNumber] == i - 1)
+                {
+                    lastNumber = 0;
+                }
+                else
+                {
+                    var newNumber = i - 1 - numbers[lastNumber];
+                    numbers[lastNumber] = i - 1;
+                    numbers[newNumber] = i;
+                    lastNumber = newNumber;
+                }
+            }
+
+            return lastNumber.ToString();
+        }
+    }
+}
---
+++ Tests/Day15Tests.cs
@@ -0,0 +1,67 @@
+﻿using Aoc2020;
+using Aoc2020.Day15;
+
+using Shouldly;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Tests
+{
+    public class Day15Tests
+    {
+        private Day15 day;
+
+        public Day15Tests()
+        {
+            this.day = new Day15();
+        }
+
+        [Fact]
+        public void Part1Test1()
+        {
+            day.ExecutePart1("1,3,2").ShouldBe("1");
+        }
+
+        [Fact]
+        public void Part1Test2()
+        {
+            day.ExecutePart1("2,1,3").ShouldBe("10");
+        }
+
+        [Fact]
+        public void Part1Test3()
+        {
+            day.ExecutePart1("1,2,3").ShouldBe("27");
+        }
+
+        [Fact]
+        public void Part1Test4()
+        {
+            day.ExecutePart1("2,3,1").ShouldBe("78");
+        }
+
+        [Fact]
+        public void Part1Test5()
+        {
+            day.ExecutePart1("3,2,1").ShouldBe("438");
+        }
+
+        [Fact]
+        public void Part1Test6()
+        {
+            day.ExecutePart1("3,1,2").ShouldBe("1836");
+        }
+
+        [Fact]
+        public void Part2Test1()
+        {
+            day.ExecutePart2("0,3,6").ShouldBe("175594");
+        }
+    }
+}
